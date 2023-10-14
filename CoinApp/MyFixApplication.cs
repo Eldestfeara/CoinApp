7808160f845b482c198d832703e07fd8d74ff262
Alperen Öncel <alperenoncel@gmail.com>
2023-10-14 16:07:10 +0300

    Initial commit

---
+++ CoinApp/MyFixApplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using QuickFix;
+public class MyFixApplication : MessageCracker, IApplication
+{
+    public void FromAdmin(QuickFix.Message message, SessionID sessionId) { }
+
+    public void FromApp(QuickFix.Message message, SessionID sessionId)
+    {
+        // Handle incoming application-level messages (i.e., your FIX messages)
+        Crack(message, sessionId);
+    }
+
+    public void OnCreate(SessionID sessionId) { }
+
+    public void OnLogon(SessionID sessionId) { }
+
+    public void OnLogout(SessionID sessionId) { }
+
+    public void ToAdmin(QuickFix.Message message, SessionID sessionId) { }
+
+    public void ToApp(QuickFix.Message message, SessionID sessionId) { }
+
+    public void OnMessage(QuickFix.FIX44.MarketDataSnapshotFullRefresh msg, SessionID s)
+    {
+        for (int idx = 0; idx < msg.NoMDEntries.getValue(); idx++)
+        {
+            var level = new QuickFix.FIX44.MarketDataSnapshotFullRefresh.NoMDEntriesGroup();
+            msg.GetGroup(idx + 1, level);
+
+            Console.WriteLine($"Orderbook {level.MDEntryType} @ {msg.Symbol}:");
+            Console.WriteLine($" Time: {level.MDEntryTime}");
+            Console.WriteLine($" Px: {level.MDEntryPx}");
+        }
+    }
+}
---
+++ CoinApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using QuickFix;
+using QuickFix.Transport;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Load your FIX settings from a configuration file
+        SessionSettings settings = new("quickfix.cfg");
+        IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+        ILogFactory logFactory = new FileLogFactory(settings);
+        IMessageFactory messageFactory = new DefaultMessageFactory();
+
+        IApplication application = new MyFixApplication();
+
+        var initiator = new SocketInitiator(application, storeFactory, settings, logFactory, messageFactory);
+        initiator.Start();
+
+        Console.WriteLine("Press ENTER to quit.");
+        Console.ReadLine();
+
+        initiator.Stop();
+    }
+}
+
